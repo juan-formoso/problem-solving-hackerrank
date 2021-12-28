3b26d8f6988920590df94ae990923a8dc667d9ab
Juan Formoso <trajano.juan@gmail.com>
2021-12-28 17:18:39 -0300

    Add lonely-intenger problem description

--- week-02/challenges/lonely-intenger.js
+++ week-02/challenges/lonely-intenger.js
@@ -1,3 +1,13 @@
+/* Problem Description:
+
+Given an array of integers, where all elements but one occur twice, find the unique element.
+
+Example:
+a = [1, 2, 3, 4, 3, 2, 1]
+The unique element is 4.
+
+*/
+
 function lonelyintenger(a) {
   let uniqueNum = a.filter(function (index) {
     return a.indexOf(index) === a.lastIndexOf(index);
