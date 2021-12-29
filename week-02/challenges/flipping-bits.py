33a64683918d46e9fb105920703275e3a66ae7c6
Juan Formoso <trajano.juan@gmail.com>
2021-12-29 18:06:58 -0300

    Format code on flipping bits

--- week-02/challenges/flipping-bits.py
+++ week-02/challenges/flipping-bits.py
@@ -25,22 +25,17 @@ import re
 import sys
 
 def flippingBits(n):
-    s = bin(n)[2:]
-    t = s.maketrans("01", "10")
-    s = s.translate(t)
-    s = (32-len(s))*"1"+s
-    return int(s,2)
+  s = bin(n)[2:]
+  t = s.maketrans("01", "10")
+  s = s.translate(t)
+  s = (32-len(s))*"1"+s
+  return int(s,2)
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    q = int(input().strip())
-
-    for q_itr in range(q):
-        n = int(input().strip())
-
-        result = flippingBits(n)
-
-        fptr.write(str(result) + '\n')
-
-    fptr.close()
+  fptr = open(os.environ['OUTPUT_PATH'], 'w')
+  q = int(input().strip())
+  for q_itr in range(q):
+    n = int(input().strip())
+    result = flippingBits(n)
+    fptr.write(str(result) + '\n')
+  fptr.close()
