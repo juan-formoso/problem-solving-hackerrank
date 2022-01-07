d0859b65545069e1fb9a928c4a1e0b375faafd00
Juan Formoso <trajano.juan@gmail.com>
2022-01-07 10:12:35 -0300

    Add xor solution

--- week-03/challenges/xor-strings.py
+++ week-03/challenges/xor-strings.py
@@ -1,10 +1,13 @@
+"""
+"""
+
 def strings_xor(s, t):
   res = ""
   for i in range(len(s)):
-    if s[i] = t[i]:
-      res = '0';
+    if s[i] == t[i]:
+      res += '0';
     else:
-      res = '1';
+      res += '1';
 
   return res
 
