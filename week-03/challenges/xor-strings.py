414cbafa6ef122300efe80400e8901913a7f6a52
Juan Formoso <trajano.juan@gmail.com>
2022-01-07 10:14:05 -0300

    Add xor strings description

--- week-03/challenges/xor-strings.py
+++ week-03/challenges/xor-strings.py
@@ -1,4 +1,16 @@
 """
+In this challenge, the task is to debug the existing code to successfully execute all provided test files.
+
+Given two strings consisting of digits 0 and 1 only, find the XOR of the two strings.
+
+Note: You can modify at most three lines in the given code and you cannot add or remove lines to the code.
+
+Sample Input
+10101
+00101
+
+Sample Output
+10000
 """
 
 def strings_xor(s, t):
